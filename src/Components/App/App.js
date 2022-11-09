1e1ecd9d7765389a8411290afc0ad235ab3ad58d
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-11-09 09:42:53 -0600

    Install PropTypes and begin App skeleton

---
+++ src/Components/App/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './App.css'
+
+export default class App extends Component {
+    constructor() {
+        super()
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div>Hi</div>
+        )
+    }
+}

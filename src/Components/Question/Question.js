208002495210b30997996efbb116237852406036
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-11-10 15:07:40 -0600

    Start work on updating currentQuestion

--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Question.css'
 
 export const Question = ({ currentQuestion }) => {
-    // category, type, question, correct_answer, incorrect_answer, difficulty
+    // currentQuestion properties: category, type, question, correct_answer, incorrect_answer, difficulty
+    const [allAnswers, setAllAnswers] = useState([])
+
+    useEffect(() => {
+        const totalAnswers = currentQuestion.incorrect_answers.map(answer => answer)
+        const mixAnswers = Math.floor(Math.random() * Math.floor(totalAnswers.length))
+        totalAnswers.splice(mixAnswers, 0, currentQuestion.correct_answer)
+        setAllAnswers(totalAnswers)
+    }, [currentQuestion])
+
+    // console.log(currentQuestion.correct_answer)
+    // console.log(currentQuestion.incorrect_answers)
+    // console.log(allAnswers)
 
     return (
         <article className='question-card'>
             <h3 className='question-text'>{currentQuestion.question}</h3>
-            <p className='question-text'>{currentQuestion.correct_answer}</p>
-            <p className='question-text'>{currentQuestion.incorrect_answers.join(' ')}</p>
+            <h4 className='question-text'>{allAnswers}</h4>
             <span className='btn-styling'>
                 <button className='question-btn'>Save for Review</button>
                 <button className='question-btn'>Next Question</button>

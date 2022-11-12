c3f700c3eb2da4660d30822b71f0a9cc6a586f9e
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-11-11 18:21:15 -0600

    render answer buttons using map

--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -5,7 +5,8 @@ export const Question = ({ currentQuestion }) => {
 
     const [allAnswers, setAllAnswers] = useState([])
     const [score, setScore] = useState(0)
-    const [thisQuestion, setThisQuestion] = useState({})
+    const [selectedAnswer, setSelectedAnswer] = useState('')
+    // const [thisQuestion, setThisQuestion] = useState({})
     
     useEffect(() => { 
         const totalAnswers = currentQuestion.incorrect_answers.map(answer => answer)
@@ -16,34 +17,42 @@ export const Question = ({ currentQuestion }) => {
 
     console.log('Correct Answer:', currentQuestion.correct_answer)
     
-    const clickAnswer = (event) => {
-        const answer = event.target.value
-        console.log('You clicked: Correct Answer:', currentQuestion.correct_answer)
-        console.log('What you clicked:', answer)
-        if (answer === currentQuestion.correct_answer) {
-            event.currentTarget.classList.add('answer-correct')
+    const clickAnswer = (value) => {
+        setSelectedAnswer(value)
+        if (value === currentQuestion.correct_answer) {
+            // event.currentTarget.classList.add('answer-correct')
             setScore(prevScore => prevScore + 1)
         } else {
-            event.currentTarget.classList.add('answer-incorrect')
+            // event.currentTarget.classList.add('answer-incorrect')
         }
     }
 
-    const getNextQuestion = () => {
+    const renderButtons = () => {
+        return allAnswers.map(answer => {
+            let btnClass
+            if (selectedAnswer && selectedAnswer === answer) {
+                if (answer === currentQuestion.correct_answer) {
+                    btnClass = 'answer-correct'
+                } else {
+                    btnClass = 'answer-incorrect'
+                }
+            }
+            return <button disabled={selectedAnswer} className={`answer-text ${btnClass}`} value={answer} onClick={() => clickAnswer(answer)} >{answer}</button>
+        })
+    }
+
+    const getNextQuestion = (event) => {
         // how to take currentQuestion(questions[0]) to currentQuestion(questions[1])
     }
 
     return (
         <article className='question-card'>
             <h3 className='question-text'>{currentQuestion.question}</h3>
-            <button className='answer-text' value={allAnswers[0]} id='a1' onClick={clickAnswer} >{allAnswers[0]}</button>
-            <button className='answer-text' value={allAnswers[1]} id='a2' onClick={clickAnswer} >{allAnswers[1]}</button>
-            <button className='answer-text' value={allAnswers[2]} id='a3' onClick={clickAnswer} >{allAnswers[2]}</button>
-            <button className='answer-text' value={allAnswers[3]} id='a4' onClick={clickAnswer} >{allAnswers[3]}</button>
+            {renderButtons()}
             <p className='question-text'>{score ? `Score: ${score}` : `Score: 0` }</p>
             <span className='btn-styling'>
                 <button className='question-btn'>Save for Review</button>
                 <button 
-                    disabled
                     className='question-btn'
                     onClick={getNextQuestion}
                 >Next Question</button>

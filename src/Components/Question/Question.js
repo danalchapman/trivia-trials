d5ed95e564f749cce06d91268ecf63001fd78df7
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-11-13 16:15:37 -0600

    Slight refactoring for Lighthouse accessibility

--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -57,7 +57,7 @@ export const Question = ({ currentQuestion, questionIndex, setQuestionIndex, add
 
     return (
         <article className='question-card'>
-            <h3 className='question-text'>{thisQuestion.replaceAll('&quot;', '').replaceAll('&amp;', '&').replaceAll('&#039;', '\'')}</h3>
+            <p className='question-text'>{thisQuestion.replaceAll('&quot;', '').replaceAll('&amp;', '&').replaceAll('&#039;', '\'')}</p>
             {renderButtons()}
             <div className='btn-styling'>
                 <button 

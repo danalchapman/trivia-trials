3f889d1c32aad7e377b1f579253fc2e7fcc252b5
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-11-12 20:07:07 -0600

    Start trivia_spec tests

--- cypress/e2e/trivia_spec.cy.js
+++ cypress/e2e/trivia_spec.cy.js
@@ -1,5 +1,22 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Trivia', () => {
+
+  beforeEach(() => {
+    cy.intercept('GET', 'https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple', {
+      statusCode: 200,
+      ok: true,
+      body: {
+        "question": "Test Question",
+        "correct_answer": "Yes",
+        "incorrect_answers": ["No", "Maybe", "Kinda"],
+        "category": "Test",
+        "type": "multiple",
+        "difficulty": "easy"
+      }
+    })
+    cy.visit('http://localhost:3000/trivia')
+  })
+
+  it('Should be able to render the elements of a question', () => {
+
   })
 })

51227c6daacc41350c4a1bec12dc18292e8b7075
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-11-12 22:27:45 -0600

    Continue adding tests and take note of which ones aren't working

--- cypress/e2e/review_spec.cy.js
+++ cypress/e2e/review_spec.cy.js
@@ -1,5 +1,33 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Review page', () => {
+
+  beforeEach(() => {
+    cy.intercept('GET', 'https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple', {
+      statusCode: 200,
+      ok: true,
+      body: [{
+        "question": "Test Question",
+        "correctAnswer": "Yes",
+        "incorrectAnswers": ["No", "Maybe", "Kinda"]
+      }]
+     })
   })
+  
+  it('Should have a message if there are no saved questions', () => {
+    cy.visit('http://localhost:3000/review')
+    cy.get('h2').contains('Turn back... there is nothing for you here.')
+    cy.get('.return-home').contains('Return to the surface...').click()
+    cy.url().should('eq', 'http://localhost:3000/')
+  })
+
+  it('Should be able to return home from the review page', () => {
+    cy.visit('http://localhost:3000/review')
+    cy.get('nav').contains('Home').click()
+    cy.url().should('eq', 'http://localhost:3000/')
+  })
+  
+  // it('Should a user delete a saved question, it disappears from the DOM', () => {
+    
+
+  // })
+
 })
--- cypress/e2e/trivia_spec.cy.js
+++ cypress/e2e/trivia_spec.cy.js
@@ -1,22 +1,38 @@
-describe('Trivia', () => {
+// describe('Trivia page', () => {
 
-  beforeEach(() => {
-    cy.intercept('GET', 'https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple', {
-      statusCode: 200,
-      ok: true,
-      body: {
-        "question": "Test Question",
-        "correct_answer": "Yes",
-        "incorrect_answers": ["No", "Maybe", "Kinda"],
-        "category": "Test",
-        "type": "multiple",
-        "difficulty": "easy"
-      }
-    })
-    cy.visit('http://localhost:3000/trivia')
-  })
+//   it('Should be able to render the elements of a question', () => {
+//     cy.intercept('GET', 'https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple', {
+//       statusCode: 200,
+//       ok: true,
+//       body: {
+//         "question": "Test Question",
+//         "correct_answer": "Yes",
+//         "incorrect_answers": ["No", "Maybe", "Kinda"],
+//         "category": "Test",
+//         "type": "multiple",
+//         "difficulty": "easy"
+//       }
+//     })
+//     cy.visit('http://localhost:3000/trivia')
 
-  it('Should be able to render the elements of a question', () => {
+//     cy.get('h3.question-text')
+//     cy.get('[value="Yes"]')
+//     cy.get('[value="No"]')
+//     cy.get('[value="Maybe"]')
+//     cy.get('[value="Kinda"]')
+//     cy.get('.btn-styling > :nth-child(1)')
+//     cy.get('[disabled=""]')
+//     cy.get('p.question-text')
+//   })
+// })
 
-  })
-})
+// describe('Failed Request', () => {
+//   it('Should display an error message if the GET request fails', () => {
+//     cy.intercept('GET', 'https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple', {
+//       statusCode: 404,
+//       ok: false
+//     })
+//     cy.visit('http://localhost:3000/trivia')
+//     cy.get('.error-msg').contains('Oops. 404. Try again.')
+//   })
+// })

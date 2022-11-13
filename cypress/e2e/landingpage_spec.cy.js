12667573838be76b6a2619f1bd4a937ffc5fc6a1
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-11-12 19:57:31 -0600

    Write most of the landpage_spec tests

--- cypress/e2e/landingpage_spec.cy.js
+++ cypress/e2e/landingpage_spec.cy.js
@@ -1,6 +1,7 @@
 describe('Landing Page', () => {
+
   beforeEach(() => {
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:3000/')
   })
 
   it('Should render the title', () => {
@@ -25,9 +26,49 @@ describe('Landing Page', () => {
   })
 })
 
-// Navigation
-// should be able to click the links and see the URL update
+describe('Navigation', () => {
 
-// Form
-// Should set the difficulty and be taken to the first question
-// intercept/stub first question
+  it('Should be able to click on the links and see the URL update', () => {
+    cy.visit('http://localhost:3000/')
+      .contains('Review').click()
+    cy.url('http://localhost:3000/review')
+
+    cy.contains('Home').click()
+    cy.url('http://localhost:3000/')
+  })
+})
+
+// describe('Difficulty Form', () => {
+
+//   it('Should be able to pick a difficulty and start the game', () => {
+//     cy.visit('http://localhost:3000/')
+//     cy.intercept('GET', '/trivia', {
+//       statusCode: 200,
+//       ok: true,
+//       body: {
+//         "question": "Test Question",
+//         "correct_answer": "Yes",
+//         "incorrect_answers": ["No", "Maybe", "Kinda"],
+//         "category": "Test",
+//         "type": "multiple",
+//         "difficulty": "easy"
+//       }
+//     })
+//     cy.get('.difficulty-dropdown')
+//       .select('easy')
+//       .invoke('val')
+//       .should('eq', 'easy')
+//     cy.get('.submit-difficulty').contains('Enter the Dungeon').click()
+//     cy.url('http://localhost:3000/trivia')
+//   })
+// })
+
+describe('BadURL', () => {
+
+  it('Should be able to handle a bad URL', () => {
+    cy.visit('http://localhost:3000/badUrl')
+    cy.get('h2').contains('Halt! You have made an error in your path...')
+    cy.get('.home-button').contains('Go back towards the light..').click()
+    cy.url().should('eq', 'http://localhost:3000/')
+  })
+})

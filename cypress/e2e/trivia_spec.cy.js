8a7222bf7abae3d6ddc7d0383bcc8500e4e0aaff
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-11-13 14:58:54 -0600

    Finish trivia spec tests

--- cypress/e2e/trivia_spec.cy.js
+++ cypress/e2e/trivia_spec.cy.js
@@ -1,38 +1,32 @@
-// describe('Trivia page', () => {
+describe('Trivia page', () => {
 
-//   it('Should be able to render the elements of a question', () => {
-//     cy.intercept('GET', 'https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple', {
-//       statusCode: 200,
-//       ok: true,
-//       body: {
-//         "question": "Test Question",
-//         "correct_answer": "Yes",
-//         "incorrect_answers": ["No", "Maybe", "Kinda"],
-//         "category": "Test",
-//         "type": "multiple",
-//         "difficulty": "easy"
-//       }
-//     })
-//     cy.visit('http://localhost:3000/trivia')
+  it.only('Should be able to render the elements of a question', () => {
+    cy.intercept('https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple', {
+      statusCode: 200,
+      ok: true,
+      fixture: 'game1.json'
+    })
+    cy.visit('http://localhost:3000/trivia')
 
-//     cy.get('h3.question-text')
-//     cy.get('[value="Yes"]')
-//     cy.get('[value="No"]')
-//     cy.get('[value="Maybe"]')
-//     cy.get('[value="Kinda"]')
-//     cy.get('.btn-styling > :nth-child(1)')
-//     cy.get('[disabled=""]')
-//     cy.get('p.question-text')
-//   })
-// })
+    cy.get('h3.question-text').should('exist').contains('Which of the following Arab countries does NOT have a flag containing only Pan-Arab colours?')
+    cy.get('[value="Kuwait"]').should('exist').contains('Kuwait')
+    cy.get('[value="Qatar"]').should('exist').contains('Qatar')
+    cy.get('[value="United Arab Emirates"]').should('exist').contains('United Arab Emirates')
+    cy.get('[value="Jordan"]').should('exist').contains('Jordan')
+    cy.get('.btn-styling > :nth-child(1)').should('exist').contains('Save for Review')
+    cy.get('[disabled=""]').should('exist').contains('Next Question')
+    cy.get('p.question-text').should('exist').contains('Score: 0/5')
+  })
+})
 
-// describe('Failed Request', () => {
-//   it('Should display an error message if the GET request fails', () => {
-//     cy.intercept('GET', 'https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple', {
-//       statusCode: 404,
-//       ok: false
-//     })
-//     cy.visit('http://localhost:3000/trivia')
-//     cy.get('.error-msg').contains('Oops. 404. Try again.')
-//   })
-// })
+describe('Failed Request', () => {
+
+  it('Should display an error message if the GET request fails', () => {
+    cy.intercept('https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple', {
+      statusCode: 404,
+      ok: false
+    })
+    cy.visit('http://localhost:3000/trivia')
+    cy.get('.error-msg').contains('Oops. 404 Try again.')
+  })
+})
